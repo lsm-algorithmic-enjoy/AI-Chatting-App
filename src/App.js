f73b33d7afcc75a68ab7a6b75883936d2199e386
seungmin-lee-dotcom <k07217@naver.com>
2023-01-08 13:27:52 +0900

    chatting-clear

--- src/App.js
+++ src/App.js
@@ -1,25 +1,103 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { TbSend } from "react-icons/tb";
+import { AiTwotoneMessage } from "react-icons/ai";
+import { RiSunLine } from "react-icons/ri";
 
-function App() {
+export default function App() {
+  const [chatbotmessageList, setChatbotMessageList] = useState([]);
+  const [usermessageList, setUserMessageList] = useState([]);
+  const [message, setMessage] = useState("");
+  const chatbotreadList = async () => {
+    const { data } = await axios.get("http://localhost:9000/chatbot");
+    setChatbotMessageList(data);
+  };
+  const userreadList = async () => {
+    const { data } = await axios.get("http://localhost:9000/user");
+    setUserMessageList(data);
+  };
+
+  const resetDB = async () => {
+    const useridList = await axios("http://localhost:9000/user").then(
+      ({ data }) => data.map((item) => item.id)
+    );
+    const chatbotidList = await axios("http://localhost:9000/chatbot").then(
+      ({ data }) => data.map((item) => item.id)
+    );
+    await Promise.all(
+      useridList.map((id) => axios.delete(`http://localhost:9000/user/${id}`)),
+      chatbotidList.map((id) =>
+        axios.delete(`http://localhost:9000/chatbot/${id}`)
+      )
+    );
+  };
+
+  useEffect(() => {
+    (async () => {
+      await chatbotreadList();
+      await userreadList();
+    })();
+  }, []);
+
+  const handleSubmit = async () => {
+    axios.post("http://localhost:9000/user", {
+      message,
+    });
+    const { data } = await axios.post(
+      "https://main-chatbot-api-ainize-team.endpoint.ainize.ai/v1/bot/chat",
+      {
+        message,
+      }
+    );
+    await axios.post("http://localhost:9000/chatbot", {
+      message: data,
+    });
+    userreadList();
+    chatbotreadList();
+    setMessage("");
+  };
+
+  const handleClear = () => {
+    resetDB();
+    userreadList();
+    chatbotreadList();
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="holebox">
+      <div style={{ color: "#A56BEE" }}>
+        <AiTwotoneMessage />
+        AI chat
+      </div>
+      <div className="container">
+        <div className="chattings">
+          <span>
+            {chatbotmessageList.map((msg1) => (
+              <div key={msg1.id}>Chatbot: {msg1.message}</div>
+            ))}
+          </span>
+          <br />
+          <span>
+            {usermessageList.map((msg2) => (
+              <div key={msg2.id}>Human: {msg2.message}</div>
+            ))}
+          </span>
+        </div>
+        <div className="footer">
+          <input
+            className="bottominput"
+            placeholder="Write Your Message..."
+            type="text"
+            value={message}
+            onChange={(event) => setMessage(event.target.value)}
+          />
+          <TbSend onClick={handleSubmit} className="sendicon" />
+        </div>
+      </div>
+      <span onClick={handleClear} style={{ cursor: "pointer" }}>
+        <RiSunLine />
+        Clear Conversation
+      </span>
     </div>
   );
 }
-
-export default App;
